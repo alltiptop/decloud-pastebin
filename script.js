d72553180c950662205a04f3c7b19f24a2f5dfff
Evgeny Yulin <mail@alltiptop.me>
2018-03-18 23:01:02 +0400

    Refractoring

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 "use strict"
 
-let startLine = 1
-
 const form = document.getElementById('form'),
-      textarea = document.getElementById('textarea'),
-      numbers = document.getElementById('lines'),
       code = document.getElementById('code'),
       showButton = document.getElementById('showImage'),
       hideButton = document.getElementById('hideImage')
@@ -13,98 +9,126 @@ const canvasOptions = { backgroundColor: null }
 
 const getLines = (string = "") => string.split(/\r\n|\r|\n/)
 
-const resize = (value = textarea.value, item = textarea) => {
-  let height = getLines(value).length,
-      index = new Number(startLine)
-  while (numbers.firstChild) {
-    numbers.removeChild(numbers.firstChild)
-  }
-  getLines(value).forEach((line, i) => {
-    const newLine = document.createElement('div')
-    newLine.innerHTML = index
-    numbers.appendChild(newLine)
-    let lineLength = Math.floor(line.length / 80)
-    if (Math.floor(line.length / 80) > 0) {
-      for (let i = 0; i < lineLength; i++) {
-        height++
-        const clearLine = document.createElement('div')
-        numbers.appendChild(clearLine)
-      }
+const hotkeys = {
+  check ({ which }) {
+    console.log(which)
+    if (!which) return
+    switch (which) {
+      case 9:
+        this.tab(event)
+        break;
+      default:
+        return
     }
-    index ++
-  })
-  item.style.height = height * 20 + 20 + 'px'
+  },
+  tab: (event) => {
+    event.preventDefault()
+    const { selectionStart, selectionEnd, value } = event.target
+    event.target.value = value.substring(0, selectionStart) +
+                         '    ' +
+                         value.substring(selectionEnd)
+    event.selectionStart = selectionStart + 4
+    event.selectionEnd = selectionEnd + 4
+  }
 }
 
-const updateCode = (value = textarea.value) => {
-  const colorized = self.hljs.highlightAuto(value)
-  code.innerHTML = colorized.value
+const editor = {
+  resize: (value = textarea.value, item = textarea) => {
+    let height = getLines(value).length
+    lines.update(value)
+    getLines(value).forEach((line, i) => {
+      let lineLength = Math.floor(line.length / 80)
+      for (let i = 0; i < lineLength; i++) height++
+    })
+    item.style.height = height * 20 + 20 + 'px'
+  },
+  updateCode: (value = "") => {
+    const colorized = self.hljs.highlightAuto(value)
+    code.innerHTML = colorized.value
+  },
+  update (event) {
+    event.target.value = event.target.value.replace(/\t/g, '    ')
+    const { value } = event.target
+    this.resize(value, textarea)
+    this.updateCode(value)
+  }
 }
 
 window.onload = () => {
-  resize()
-  updateCode()
+  const textarea = document.getElementById('textarea')
+  editor.updateCode(textarea.value)
+  editor.resize(textarea.value)
 }
 
-textarea.oninput = (event) => {
-  textarea.value = event.target.value.replace(/\t/g, '    ')
-  const value = new String(event.target.value)
-  resize(value, textarea)
+/*
+ * Image getting
+ */
 
-  if (event.which === 9) {
-    event.preventDefault()
-    const { selectionStart, selectionEnd } = event.target
-    textarea.value = value.substring(0, selectionStart) +
-                     '    ' +
-                     value.substring(selectionEnd)
-    textarea.selectionStart = selectionStart + 4
-    textarea.selectionEnd = selectionEnd + 4
+const image = {
+  img: document.createElement("img"),
+  checkImg () {
+    if (form.contains(this.img)) form.removeChild(this.img)
+  },
+  show (event) {
+    showButton.style.display = 'none'
+    hideButton.style.display = 'block'
+    this.checkImg()
+    html2canvas(form, canvasOptions).then(canvas => {
+      this.img.src = canvas.toDataURL("image/png")
+      this.img.classList = 'codeImage'
+      form.appendChild(this.img)
+    })
+  },
+  hide (event) {
+    this.checkImg()
+    showButton.style.display = 'block'
+    hideButton.style.display = 'none'
+  },
+  save () {
+    this.checkImg()
+    html2canvas(form, canvasOptions).then(canvas => {
+      const link = document.createElement("a")
+      link.download = 'code.png'
+      link.href = canvas.toDataURL("image/png")
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    });
   }
-  updateCode(value)
 }
 
 /*
- * Get image
+ * Lines editor
  */
 
-const image = document.createElement("img")
-
-const showImage = (event) => {
-  showButton.style.display = 'none'
-  hideButton.style.display = 'block'
-  if (form.contains(image)) form.removeChild(image)
-  html2canvas(form, canvasOptions).then(canvas => {
-    image.src = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
-    image.classList = 'codeImage'
-    form.appendChild(image)
-  })
-}
-
-const hideImage = (event) => {
-  showButton.style.display = 'block'
-  hideButton.style.display = 'none'
-  if (form.contains(image)) form.removeChild(image)
-}
-
-const saveImage = () => {
-  if (form.contains(image)) form.removeChild(image)
-  html2canvas(form, canvasOptions).then(canvas => {
-    const link = document.createElement("a")
-    link.download = 'code.png'
-    link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-  });
-}
-
-/*
- * Set start line
- */
-
-const setStartLine = (event) => {
-  const { value } = event.target
-  if (!/^\+?(0|[1-9]\d*)$/.test(value)) return
-  startLine = value
-  resize()
+const lines = {
+  start: 1,
+  container: document.getElementById('lines'),
+  block: (number = null) => {
+    const el = document.createElement('div')
+    el.innerHTML = number
+    return el
+  },
+  update (value = "") {
+    while (this.container.firstChild) {
+      this.container.removeChild(this.container.firstChild)
+    }
+    let index = Number(this.start)
+    getLines(value).forEach((line, i) => {
+      let newLine = this.block(index)
+      this.container.appendChild(newLine)
+      let lineLength = Math.floor(line.length / 80)
+      for (let i = 0; i < lineLength; i++) {
+        const clearLine = this.block()
+        this.container.appendChild(clearLine)
+      }
+      index ++
+    })
+  },
+  setStart (event) {
+    const { value } = event.target
+    if (!/^\+?([0-9]\d*)$/.test(value)) return
+    this.start = value
+    editor.resize()
+  }
 }
